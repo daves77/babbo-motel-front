92dc830ca6ad3e8c265ffa560aa654f88451e168
daves <davidlimjh77@gmail.com>
2022-03-25 05:36:50 +0800

    working state

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -23,11 +23,11 @@ export default function TextBox () {
 
   if (message) {
     return (
-			<div className='p-4 absolute left-0 w-full bottom-0 h-9 text-xs rounded-b bg-slate-500'>
+			<div className='p-4 absolute left-0 w-full bottom-0 h-9 text-xs rounded-b bg-darkblue/[0.8]'>
 				<div className='absolute w-full top-0 left-0 p-1 px-2'>
           <div className="flex justify-between">
-					<p className=''>{message}</p>
-					<button type='button' className="justify-self-end" onClick={closeTextMessage}>
+					<p className='text-white'>{message}</p>
+					<button type='button' className="justify-self-end text-white" onClick={closeTextMessage}>
 						Next
 					</button>
           </div>
